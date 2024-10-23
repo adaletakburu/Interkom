94e8504eb6d91694b3a64f3d33c74fede279df74
Adalet AKBURU <adalet.akburu@hotmail.com>
2024-10-23 16:47:01 +0300

    SIPSorcery library added and target framework,os version updated

--- Infrastructure/Interkom.Infrastructure.Persistence/Services/ZenitelInterkomService.cs
+++ Infrastructure/Interkom.Infrastructure.Persistence/Services/ZenitelInterkomService.cs
@@ -1,16 +1,30 @@
 ﻿using Interkom.Core.Application.Interfaces;
+using SIPSorcery.Media;
+using SIPSorcery.SIP.App;
+using SIPSorceryMedia.Windows;
 
 namespace Interkom.Infrastructure.Persistence.Services
 {
     public class ZenitelInterkomService : IZenitelIntercomService
     {
-        public Task CallAsync()
+        static string DESTINATION = "10.1.58.85";
+        static SIPUserAgent userAgent = new SIPUserAgent();
+
+        public async Task CallAsync()
         {
+            var winAudioEndPoint = new WindowsAudioEndPoint(new AudioEncoder());
+            var voipMediaSession = new VoIPMediaSession(winAudioEndPoint.ToMediaEndPoints());
+
+            bool callResult = await userAgent.Call(DESTINATION, null, null, voipMediaSession);
+
+            Console.WriteLine("Press any key to exit...");
+            //Console.ReadLine();
             throw new NotImplementedException();
         }
 
         public Task ClientsAsync()
         {
+            userAgent.Hangup();//HANGUP
             throw new NotImplementedException();
         }
 
